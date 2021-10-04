4454696fedae00be3087f5540e006a8f3b6c181b
colton305 <colton@pambra.ca>
2021-10-04 17:04:43 -0600

    Added course class to save courses to db

---
+++ course.py
@@ -0,0 +1,65 @@
+
+class Course:
+
+    def __init__(self, subject, soup):
+        self.subject = subject
+        header = soup.find("h2", class_="m-0").text
+        self.course_id = header.split("-")[0].strip()
+        self.description = header.split("-")[1].strip()
+        self.credits = soup.find("h5", class_="mt-0").text.split(" ")[1]
+        long_text = soup.findAll("p")[1].text
+        prereq_position = long_text.find("Prerequisite")
+        if prereq_position == -1:
+            self.prereqs = None
+        else:
+            # Temporary prereq determination, PLS FIX LATER
+            self.prereqs = []
+            prereq_string = long_text[prereq_position:]
+            start = 0
+            end = 0
+            for i, char in enumerate(prereq_string):
+                if char == ":":
+                    start = i
+                elif char == ".":
+                    end = i
+                    break
+            prereq_string = prereq_string[start + 2: end]
+            prereq_list = prereq_string.split(" ")
+            for i, word in enumerate(prereq_list):
+                if i == 0:
+                    subject = word
+                else:
+                    if len(word) > 3:
+                        word = word[:4]
+                    try:
+                        class_num = int(word)
+                    except ValueError:
+                        class_num = 0
+                    break
+            if class_num == 0:
+                self.prereqs = None
+            else:
+                self.prereqs.append(subject + " " + str(class_num))
+
+    def print_stuff(self):
+        print(self.subject)
+        print(self.course_id)
+        print(self.description)
+        print(self.credits)
+        print(self.prereqs)
+
+    # Generate a dictionary of all of the course variables for saving to the database
+    def __generate_dict(self):
+        json = {"_id": self.course_id, "description": self.description, "credits": self.credits,
+                "prerequisites": self.prereqs}
+        return json
+
+    # Save the course to the database
+    def save_to_db(self, collection):
+        current_courses = collection.find({"_id": self.course_id})
+        exists = False
+        for _class in current_courses:
+            exists = True
+        if exists:
+            return
+        collection.insert_one(self.__generate_dict())
--- main.py
+++ main.py
@@ -4,25 +4,35 @@ import requests
 from bs4 import BeautifulSoup
 
 from _class import _Class
+from course import Course
 
 HEADERS = {"User-Agent": "APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)"}
-RESPONSE = requests.get("https://apps.ualberta.ca/catalogue/course/biol/107", headers=HEADERS)
+RESPONSE = requests.get("https://apps.ualberta.ca/catalogue/course/biol", headers=HEADERS)
 SOUP = BeautifulSoup(RESPONSE.content, features="html.parser")
 
 
 def main():
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["scheduling"]
-    collection = db["classes"]
+    course_collection = db["courses"]
+    class_collection = db["classes"]
 
-    classes = []
-    for card in SOUP.findAll("div", class_="card mt-4 dv-card-flat"):
-        semester = card.find("h4", class_="m-0 flex-grow-1").text.split(" ")[0]
-        for div in card.findAll("div", class_="col-lg-4 col-12 pb-3"):
-            classes.append(_Class("BIOL 107", semester, div))
+    courses = []
+    for button in SOUP.findAll("a", class_="btn btn-sm btn-secondary"):
+        url = "https://apps.ualberta.ca/" + button["href"]
+        course_response = requests.get(url, headers=HEADERS)
+        course_soup = BeautifulSoup(course_response.content, features="html.parser")
+        courses.append(Course("BIOL", course_soup))
+        classes = []
+        for card in course_soup.findAll("div", class_="card mt-4 dv-card-flat"):
+            semester = card.find("h4", class_="m-0 flex-grow-1").text.split(" ")[0]
+            for div in card.findAll("div", class_="col-lg-4 col-12 pb-3"):
+                classes.append(_Class(courses[-1].course_id, semester, div))
 
-    for _class in classes:
-        _class.save_to_db(collection)
+        for _class in classes:
+            _class.save_to_db(class_collection)
+    for course in courses:
+        course.save_to_db(course_collection)
 
 
 if __name__ == "__main__":
