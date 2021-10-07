ddcf04bcd7c8c094d594a47ab713d55ad422a3f3
colton305 <colton@pambra.ca>
2021-10-07 09:51:25 -0600

    Added packages that were utilized with their respective licenses in the __init__ file

--- database_construction/__init__.py
+++ database_construction/__init__.py
@@ -1,3 +1,9 @@
+# A module that is used to construct the database
+
+# LICENSING:
+# MongoDB (open-source)
+# requests (Apache 2.0 License)
+# bs4 (MIT License)
 
 # Import the main function
 from .construct_database import construct_database
